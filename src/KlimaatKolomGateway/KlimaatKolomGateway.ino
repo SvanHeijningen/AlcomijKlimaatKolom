65ddf721b030febc62422aec1e2969826df6c0e1
SvanHeijningen <59994211+SvanHeijningen@users.noreply.github.com>
2020-01-17 17:04:43 +0100

    Decreased pakkages
    Minimalized pakkage size

--- src/KlimaatKolomGateway/KlimaatKolomGateway.ino
+++ src/KlimaatKolomGateway/KlimaatKolomGateway.ino
@@ -45,6 +45,98 @@ PubSubClient client(ethClient);
 
 // to track delay since last reconnection
 unsigned long lastReconnectAttempt = 0;
+unsigned long last_tx_time = 0;
+
+char id[8]; // Identifier for the MQTT connection - will set it randomly
+const char * generateID()
+{ 
+  char chars[] = "abcdefghijklmnopqrstuvwxyz";
+  randomSeed(analogRead(0));
+  int i = 0;
+  for(i = 0; i < sizeof(id) - 1; i++) {
+    id[i] = chars[random(sizeof(chars))];
+  }
+  id[sizeof(id) -1] = '\0';
+
+  return id;
+}
+
+void processRxPacket(ZBRxResponse& rx, uintptr_t) {
+    XBeeAddress64 remoteAddress = rx.getRemoteAddress64();
+    DebugSerial.print(F("Received packet from "));
+    
+    printHex(DebugSerial, remoteAddress );
+    DebugSerial.println();
+    DebugSerial.print(F("Payload size: "));
+    DebugSerial.print(rx.getDataLength());
+    DebugSerial.println();
+    Buffer b = Buffer(rx.getData(), rx.getDataLength());
+    uint8_t type = b.remove<uint8_t>();
+    if (type == 'A' ) {
+        float his_temp = b.remove<float>();
+        float his_humidity = b.remove<float>();
+        DebugSerial.print(F("His temperature:"));
+        DebugSerial.println(his_temp);
+        DebugSerial.print(F("His humidity:"));
+        DebugSerial.println(his_humidity);
+
+        if (!client.connected()) {
+          DebugSerial.print(F("Not connected"));
+          return;
+        }
+        char mac[16];
+        byte* addr = (byte*)&remoteAddress;
+        sprintf(mac, "%.2X%.2X%.2X%.2X%.2X%.2X%.2X%.2X", addr[3], addr[2], addr[1], addr[0], addr[7], addr[6], addr[5], addr[4]);
+
+        publish(TEMP_RESOURCE, mac,  his_temp, WRITE);
+        publish(HUMID_RESOURCE, mac, his_humidity, WRITE);
+    }
+}
+
+// publishes data to the specified resource
+void publish(const char* resource, const char* mac, float data, bool persist)
+{
+  
+  char resource_with_mac[32];
+  if( mac != NULL)
+    sprintf(resource_with_mac, "%s_%s", mac, resource);
+  else 
+    sprintf(resource_with_mac, "%s", resource);
+    
+  StaticJsonBuffer<100> jsonOutBuffer;
+  JsonObject& root = jsonOutBuffer.createObject();
+  root["channel"] = CHANNEL;
+  root["resource"] = resource_with_mac;
+  if (persist) {
+    root["write"] = true;
+  }
+  root["data"] = data;
+
+  // Now print the JSON into a char buffer
+  char buffer[100];
+  root.printTo(buffer, sizeof(buffer));
+  
+  // Create the topic to publish to
+  char topic[35];
+  sprintf(topic, "%s/%s", CHANNEL, resource_with_mac);
+
+  DebugSerial.println(F("Sending to topic:"));
+  DebugSerial.println(topic);
+  DebugSerial.println(buffer);
+
+  // Now publish the char buffer to Beebotte
+  client.publish(topic, buffer);
+}
+
+
+// reconnects to Beebotte MQTT server
+boolean reconnect() {
+  if (client.connect(generateID(), TOKEN, "")) {
+    Serial.println(F("Connected to Beebotte MQTT"));
+  }
+  return client.connected();
+}
+
 
 void setup() {
   // Setup debug serial output
@@ -75,98 +167,16 @@ void setup() {
   xbee.onZBRxResponse(processRxPacket);
 }
 
-void processRxPacket(ZBRxResponse& rx, uintptr_t) {
-    XBeeAddress64 remoteAddress = rx.getRemoteAddress64();
-    DebugSerial.print(F("Received packet from "));
-    
-    printHex(DebugSerial, remoteAddress );
-    DebugSerial.println();
-    DebugSerial.print(F("Payload size: "));
-    DebugSerial.print(rx.getDataLength());
-    DebugSerial.println();
-    Buffer b = Buffer(rx.getData(), rx.getDataLength());
-    uint8_t type = b.remove<uint8_t>();
-    if (type == 'A' ) {
-        float his_temp = b.remove<float>();
-        float his_humidity = b.remove<float>();
-        DebugSerial.print(F("His temperature:"));
-        DebugSerial.println(his_temp);
-        DebugSerial.print(F("His humidity:"));
-        DebugSerial.println(his_humidity);
-
-        char mac[16];
-        byte* addr = (byte*)&remoteAddress;
-        sprintf(mac, "%.2X%.2X%.2X%.2X%.2X%.2X%.2X%.2X", addr[3], addr[2], addr[1], addr[0], addr[7], addr[6], addr[5], addr[4]);
-
-        publish(TEMP_RESOURCE, mac,  his_temp, WRITE);
-        publish(HUMID_RESOURCE, mac, his_humidity, WRITE);
-    }
-}
-
-// publishes data to the specified resource
-void publish(const char* resource, const char* mac, float data, bool persist)
-{
-  char resource_with_mac[64];
-  sprintf(resource_with_mac, "%s_%s", mac, resource);
-  StaticJsonBuffer<128> jsonOutBuffer;
-  JsonObject& root = jsonOutBuffer.createObject();
-  root["channel"] = CHANNEL;
-  root["resource"] = resource_with_mac;
-  if (persist) {
-    root["write"] = true;
-  }
-  root["data"] = data;
-
-  // Now print the JSON into a char buffer
-  char buffer[128];
-  root.printTo(buffer, sizeof(buffer));
-  
-  // Create the topic to publish to
-  char topic[44];
-  sprintf(topic, "%s/%s", CHANNEL, resource_with_mac);
-
-  DebugSerial.println("Sending to topic:");
-  DebugSerial.println(topic);
-  DebugSerial.println(buffer);
-
-  // Now publish the char buffer to Beebotte
-  client.publish(topic, buffer);
-}
-
-
-// reconnects to Beebotte MQTT server
-boolean reconnect() {
-  if (client.connect(generateID(), TOKEN, "")) {
-    Serial.println("Connected to Beebotte MQTT");
-  }
-  return client.connected();
-}
-
-
-char id[17]; // Identifier for the MQTT connection - will set it randomly
-const char * generateID()
-{ 
-  char chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-  randomSeed(analogRead(0));
-  int i = 0;
-  for(i = 0; i < sizeof(id) - 1; i++) {
-    id[i] = chars[random(sizeof(chars))];
-  }
-  id[sizeof(id) -1] = '\0';
-
-  return id;
-}
-
-
-unsigned long last_tx_time = 0;
-
 void loop() {
   // Check the serial port to see if there is a new packet available
   xbee.loop();
   // log a mark every 10 seconds
   if (millis() - last_tx_time > 10000) {
     last_tx_time = millis();
-    DebugSerial.println("Still here");
+    DebugSerial.println(F("Still here"));
+    if( client.connected()) {
+        client.publish("temp", NULL, 12.3, true);
+    }
   }
   if (!client.connected()) {
     unsigned long now = millis();
