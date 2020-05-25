4b98a91f034e45beb5ed59a3558ff15fe94f814d
Koen van Leeuwen <koen@logiqs.nl>
2020-05-25 16:47:16 +0200

    Initial checkin of serverside software to equalize the climate

---
+++ src/TemperatureEqualizer/TemperatureEqualizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TemperatureEqualizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
