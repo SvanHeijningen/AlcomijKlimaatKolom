6cff1adcb7dc6a9f5624f0c8f9d89bb7f642f617
Koen van Leeuwen <koen@logiqs.nl>
2020-06-03 11:02:27 +0200

    Make the program loop

--- src/TemperatureEqualizer/TemperatureEqualizer/Program.cs
+++ src/TemperatureEqualizer/TemperatureEqualizer/Program.cs
@@ -8,11 +8,14 @@ namespace TemperatureEqualizer
     {
         static async Task Main(string username = "you@example.user", string password = "***", string baseUri = "http://64.227.77.146:8080")
         {
-            var thingsboard = new ThingsboardHelper(baseUri, new System.Net.NetworkCredential(username, password));
-            await thingsboard.Login();
-            var equalizer = new Equalizer(thingsboard);
-            await equalizer.DoEqualizeAsync();
-
+            while (true)
+            {
+                var thingsboard = new ThingsboardHelper(baseUri, new System.Net.NetworkCredential(username, password));
+                await thingsboard.Login();
+                var equalizer = new Equalizer(thingsboard);
+                await equalizer.DoEqualizeAsync();
+                await Task.Delay(TimeSpan.FromMinutes(10));
+            }
         }
     }
 }
